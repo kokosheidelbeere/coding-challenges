d22042786ebbf7ca2bad05e03faaddae938d43c5
kokosheidelbeere <mastjura@gmail.com>
2025-02-20 14:18:37 +0100

    initial commit, fixed the obsolote syntax in setup.sh

--- crm-challenge-ts/setup.sh
+++ crm-challenge-ts/setup.sh
@@ -2,6 +2,6 @@
 
 PG_USER="postgres"
 
-docker-compose up -d
-docker-compose exec -d postgres psql -U "$PG_USER" -f /postgres/createTable.sql
-docker-compose exec -d postgres psql -U "$PG_USER" -f /postgres/importCustomers.sql
+docker compose up -d
+docker compose exec -d postgres psql -U "$PG_USER" -f /postgres/createTable.sql
+docker compose exec -d postgres psql -U "$PG_USER" -f /postgres/importCustomers.sql
